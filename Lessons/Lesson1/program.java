1d4dd90a1272936991179510f061a948fa6b10f1
Rashid Salimov <salimovrashid@yandex.ru>
2023-06-23 23:57:59 +0400

    Added Lesson1 and Seminar1

---
+++ Lessons/Lesson1/program.java
@@ -0,0 +1,19 @@
+package Lessons.Lesson1;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        System.err.println("Hello");
+
+        int a = 123;
+        a = --a - ++a;  
+        System.out.println(a); 
+    }
+    
+}
+
+
+
+
---
+++ Seminars/Seminar1/Seminar1.java
@@ -0,0 +1,19 @@
+package Seminars.Seminar1;
+
+import java.util.Scanner;
+
+// public class Seminar1 {
+//     public static void main(String[] args) {
+//         System.out.println("What is you name?");
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         sc.close();
+//         System.out.println(name);
+//     }
+// } 
+
+public class Seminar1 {
+    public static void main(String[] args) {
+        
+    }
+}
